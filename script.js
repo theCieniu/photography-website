9f7bc7c7f43e89941f15dcb557d3a60ec5c31c24
Paulina Weiher <45978460+theCieniu@users.noreply.github.com>
2020-05-28 15:02:46 +0200

    contact section
    final section css

--- script.js
+++ script.js
@@ -4,9 +4,25 @@ let b1 = document.getElementById("b1");
 let b2 = document.getElementById("b2");
 let b3 = document.getElementById("b3");
 
-burger.addEventListener("click", ()=> {
+burger.addEventListener("click", () => {
   menu.classList.toggle("mobile-menu");
   b1.classList.toggle("b1");
   b2.classList.toggle("b2");
   b3.classList.toggle("b3");
 })
+
+menu.addEventListener("click", () => {
+  menu.classList.remove("mobile-menu");
+  b1.classList.remove("b1");
+  b2.classList.remove("b2");
+  b3.classList.remove("b3");
+})
+
+let gallery = document.querySelectorAll(".gallery-section img");
+
+for (item of gallery) {
+  item.addEventListener("click", (e) => {
+    console.log(item);
+    e.target.classList.toggle("view-img");
+  })
+}
